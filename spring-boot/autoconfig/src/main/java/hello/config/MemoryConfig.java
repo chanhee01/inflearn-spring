a83ec33dcad38236c88991933a6ad5ccd3eb7101
chanhee01 <joch4267@naver.com>
2023-11-26 15:48:58 +0900

    자동 구성

---
+++ spring-boot/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,24 @@
+package hello.config;
+
+import memory.MemoryCondition;
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "memory", havingValue = "on") // memory가 on일 경우에 등록한다는 뜻
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
---
+++ spring-boot/autoconfig/src/main/java/memory/MemoryFinder.java
@@ -0,0 +1,21 @@
+package memory;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemoryFinder {
+
+    public Memory get() {
+        long max = Runtime.getRuntime().maxMemory();
+        long total = Runtime.getRuntime().totalMemory();
+        long free = Runtime.getRuntime().freeMemory(); // total 중에 사용하지 않은 메모리
+        long used = total - free;
+        return new Memory(used, max);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("init memoryFinder");
+    }
+}
